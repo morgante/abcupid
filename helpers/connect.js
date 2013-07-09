c12ca22f496c850b58b38a2c048a36809851dd95
Morgante Pell <morgante.pell@morgante.net>
2013-07-09 19:40:46 -0400

    change connect string

--- helpers/connect.js
+++ helpers/connect.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose')
 var pkg = require('../package.json')
 
 // set up Mongoose
-mongoose.connect('localhost', pkg.name);
+mongoose.connect(process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/' + pkg.name);
+
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback() {
