9be06970cf8f5afa96e25d2d30eb54cac05998a4
Morgante Pell <morgante.pell@morgante.net>
2014-04-26 05:44:08 +0400

    typo

--- okc/intellect.js
+++ okc/intellect.js
@@ -16,7 +16,7 @@ var templates = {
 	'going': 'Hey there, how\'s it going?',
 	'pretty-cool': 'Your profile is pretty cool... but are you? What is the coolest thing you\'ve done lately?',
 	'quick-question': 'Quick question - what do you expect out of OKCupid matches?',
-	'taste': 'Wow, you have incredible taste. I wish I were as cool as you... maybe you can teach me?' // sooo dumb
+	'taste': 'Wow, you have incredible taste. I wish I were as cool as you... maybe you can teach me?',
 	'standard-hello': 'Hello, how are you?'
 };
 
