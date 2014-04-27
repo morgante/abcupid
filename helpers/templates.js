bf21d063b2f647bcbcffe80484efd1d1c1c74c6b
Morgante Pell <morgante.pell@morgante.net>
2014-04-27 19:05:46 +0400

    new templates

--- helpers/templates.js
+++ helpers/templates.js
@@ -8,7 +8,10 @@ var templates = {
 	'quick-question': 'Quick question - what do you expect out of OKCupid matches?',
 	'taste': 'Wow, you have incredible taste. I wish I were as cool as you... maybe you can teach me?',
 	'standard-hello': 'Hello, how are you?',
-	'normally': 'I don\'t normally message people on this site, but when I do it\'s you. What\'s up?'
+	'normally': 'I don\'t normally message people on this site, but when I do it\'s you. What\'s up?',
+	'drinks': 'Drinks?',
+	'guys': 'Meeting any cool guys on here?',
+	'longer': 'Hey, I\'m Jim! I\'m the CTO of a media company, but I really love hiking and travel. I feel like we\'d get along well together, we should be awesome in proximity and do something fun like ride bears and fly kites. I would settle for coffee though, just in case that\'s too intense.'
 };
 
 module.exports = {
