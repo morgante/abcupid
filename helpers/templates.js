2f1500661029298eabad209eb0051f2765a5decf
Morgante Pell <morgante.pell@morgante.net>
2014-04-30 08:53:10 +0400

    disable pretty cool

--- helpers/templates.js
+++ helpers/templates.js
@@ -4,7 +4,10 @@ var templates = {
 	'sup': 'sup babe',
 	'what-new': 'What\'s new?',
 	'going': 'Hey there, how\'s it going?',
-	'pretty-cool': 'Your profile is pretty cool... but are you? What is the coolest thing you\'ve done lately?',
+	'pretty-cool': {
+		body: 'Your profile is pretty cool... but are you? What is the coolest thing you\'ve done lately?',
+		active: false
+	},
 	'quick-question': 'Quick question - what do you expect out of OKCupid matches?',
 	'taste': 'Wow, you have incredible taste. I wish I were as cool as you... maybe you can teach me?',
 	'standard-hello': 'Hello, how are you?',
