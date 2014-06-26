72f2b2a3e58d1097ebe6ca1616a03d53c7f406de
Morgante Pell <morgante.pell@morgante.net>
2014-06-26 19:08:09 -0400

    new sending system

--- okc/api2.js
+++ okc/api2.js
@@ -3,7 +3,7 @@ var _ = require('underscore');
 
 var uri = 'https://www.okcupid.com';
 var user_agent = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_6; en-US) AppleWebKit/534.16 (KHTML, like Gecko) Chrome/10.0.648.134 Safari/534.16';
-var authcodeRegex = /"authcode" : "(.+)"/;
+var authcodeRegex = /"authcode" : "(\S+)"/;
 
 exports.createClient = function() {
 	var cookies = request.jar();
@@ -70,12 +70,33 @@ exports.createClient = function() {
 						authcode = match[1];
 
 						console.log('Authcode found.');
+						console.log(authcode);
 						callback(true);
 					} else {
 						console.log('error error! could not find authcode');
 						callback(false);
+						process.exit();
 					}
 				});
+			});
+		},
+		message: function(username, message, callback) {
+			var params = {
+				sendmsg: '1',
+				r1: username,
+				body: message,
+				subject: '',
+				threadid: 0,
+				"authcode": authcode,
+				from_profile: '1',
+				ajax: '1',
+			};
+
+			get({
+				path: '/mailbox',
+				qs: params
+			}, function(err, data) {
+				callback(err, data);
 			});
 		},
 		get: get,
